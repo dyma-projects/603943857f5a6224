68bcc8cf6d5fb956a9334957690ea6be627172a8
L.Robyns <lucasrobyns@hotmail.com>
2021-04-18 19:34:53 +0200

    job done

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, Output,  EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1-enfant.component.css']
 })
 export class Exercice1EnfantComponent implements OnInit {
+  public readonly CHANGE_FACTOR = 1;
+  @Input() public counter: number;
+  @Output() public counterChangeEvent = new EventEmitter<number>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeCounterEvent(value: number) {
+    this.counterChangeEvent.emit(value);
+  }
 }
--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1.component.css']
 })
 export class Exercice1Component implements OnInit {
-  public compteur: number = 0; 
+  public compteur = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public changeCounter(value: number) {
+    this.compteur += value;
   }
 
 }
